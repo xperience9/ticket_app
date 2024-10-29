c9a2fc239702bc423977af0a687f5830e0be6f80
xperience9 <pgadams85@gmail.com>
2024-10-29 18:54:53 +0100

    new assets

--- lib/base/res/styles/app_styles.dart
+++ lib/base/res/styles/app_styles.dart
@@ -3,11 +3,26 @@ import 'package:flutter/material.dart';
 Color primary = const Color(0xFF687daf);
 
 class AppStyles {
-  static Color primaryColor = primaryColor;
+  static Color primaryColor = primary;
+  static Color bgColor = const Color(0xFFeeedf2);
   static Color textColor = const Color(0xFF3b3b3b);
+  static TextStyle textStyle = TextStyle(
+    fontSize: 16,
+    color: textColor,
+    fontWeight: FontWeight.w400,
+  );
 
-  static TextStyle headlineStyle1 =
-      TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: textColor);
+  static TextStyle headlineStyle1 = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+    color: textColor,
+  );
+
+  static TextStyle headlineStyle2 = TextStyle(
+    fontSize: 21,
+    fontWeight: FontWeight.bold,
+    color: textColor,
+  );
 
   static TextStyle headlineStyle3 = const TextStyle(
     fontSize: 17,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/res/styles/app_styles.dart';
+import 'package:ticket_app/base/widgets/app_double_text.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +11,12 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppStyles.bgColor,
       body: ListView(
         children: [
+          const SizedBox(
+            height: 40,
+          ),
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
@@ -36,22 +44,42 @@ class HomeScreen extends StatelessWidget {
                       width: 50,
                       height: 50,
                       decoration: BoxDecoration(
-                        color: Colors.blue,
+                        image: const DecorationImage(
+                          image: AssetImage(AppMedia.logo),
+                        ),
                         borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                   ],
                 ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Search icon"),
-                    Text("Empty space"),
-                  ],
+                const SizedBox(height: 25),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 12,
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xFFF4F6FD)),
+                  child: const Row(
+                    children: [
+                      Icon(
+                        FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color(0xFFBFC205),
+                      ),
+                      Text("Search"),
+                    ],
+                  ),
                 ),
+                const SizedBox(height: 40),
+                const AppDoubleText(
+                  bigText: 'Upcoming Flights',
+                  smallText: 'View all',
+                ),
+                const SizedBox(height: 40),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
